35732dcb062735ae6b5a559ce8a76ac221da3b9d
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-12-08 22:57:25 -0300

    Move some methods to `Input`

--- src/scanner/input.rs
+++ src/scanner/input.rs
@@ -7,20 +7,45 @@ pub type SpannedChar = (Span, char);
 pub struct Input<'s> {
     source: &'s str,
     chars: Peekable<CharIndices<'s>>,
+    current: SpannedChar,
 }
 
 impl<'s> Input<'s> {
     /// Creates a new input iterator.
     pub fn new(source: &'s str) -> Input<'s> {
+        let mut chars = source.char_indices().peekable();
+        let current = chars.next().map(to_spanned).unwrap_or_default();
         Input {
             source,
-            chars: source.char_indices().peekable(),
+            chars,
+            current,
         }
     }
 
+    /// Returns the current `SpannedChar`.
+    #[inline]
+    pub fn current(&self) -> SpannedChar {
+        self.current
+    }
+
+    /// Advances the input. Returns the new current char.
+    pub fn advance(&mut self) -> SpannedChar {
+        self.current = unwrap_spanned(self.chars.next().map(to_spanned), self.source.len());
+        self.current
+    }
+
     /// Peeks into the next `SpannedChar` in the iterator stream without consuming the current one.
-    pub fn peek(&mut self) -> Option<SpannedChar> {
-        self.chars.peek().copied().map(to_spanned)
+    pub fn peek(&mut self) -> SpannedChar {
+        unwrap_spanned(
+            self.chars.peek().copied().map(to_spanned),
+            self.source.len(),
+        )
+    }
+
+    /// Checks if the input is finished.
+    #[inline]
+    pub fn finished(&self) -> bool {
+        self.current.1 == '\0'
     }
 
     /// Returns the source string.
@@ -36,15 +61,14 @@ impl<'s> Input<'s> {
     }
 }
 
-impl Iterator for Input<'_> {
-    type Item = SpannedChar;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.chars.next().map(to_spanned)
-    }
-}
-
-/// Internal helper. Returns a spanned character.
+/// Returns a spanned character.
+#[inline]
 fn to_spanned((i, c): (usize, char)) -> (Span, char) {
     (Span::new(i, i + c.len_utf8()), c)
+}
+
+/// Unwraps the given `SpannedChar` with custom span.
+#[inline]
+fn unwrap_spanned(span: Option<SpannedChar>, len: usize) -> SpannedChar {
+    span.unwrap_or_else(|| (Span::new(len, len), '\0'))
 }
--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -13,7 +13,6 @@ mod input;
 
 pub struct Scanner<'s> {
     input: Input<'s>,
-    current: SpannedChar,
     lexme_lo_bound: Span,
 }
 
@@ -33,7 +32,7 @@ impl<'s> Scanner<'s> {
                 token
             });
             // Ensure that every produced token will start a new lexme.
-            self.lexme_lo_bound = self.current.0;
+            self.lexme_lo_bound = self.input.current().0;
             Some(token)
         })
     }
@@ -41,7 +40,7 @@ impl<'s> Scanner<'s> {
     /// Produces the next token.
     fn scan_token(&mut self) -> Result<Token<'s>> {
         use TokenKind::*;
-        let (span, char) = self.current;
+        let (span, char) = self.input.current();
 
         let kind = match char {
             '\0' => Eof,
@@ -63,7 +62,7 @@ impl<'s> Scanner<'s> {
             '=' => self.peek_select('=', EqualEqual, Equal),
             c if c.is_whitespace() => Whitespace(c),
             c => {
-                self.advance();
+                self.input.advance();
                 bail!(
                     "Unexpected character `{}` at position {}.",
                     human::char(c),
@@ -77,10 +76,10 @@ impl<'s> Scanner<'s> {
     /// Tries to scan a `Comment` token kind. Otherwise will return a `Slash` kind.
     fn slash_or_comment(&mut self) -> TokenKind {
         if self.peek_match('/') {
-            while !self.peek_is('\n') && !self.is_at_end() {
-                self.advance();
+            while !self.peek_is('\n') && !self.input.finished() {
+                self.input.advance();
             }
-            let lit_span = self.lexme_lo_bound.to(self.current.0).updated(2, 0);
+            let lit_span = self.lexme_lo_bound.to(self.input.current().0).updated(2, 0);
             let lit_val = self.input.spanned(lit_span).into();
             return TokenKind::Comment(lit_val);
         }
@@ -89,11 +88,14 @@ impl<'s> Scanner<'s> {
 
     /// Tries to scan a `String` token kind.
     fn string(&mut self) -> Result<TokenKind> {
-        while !self.peek_is('"') && !self.is_at_end() {
-            self.advance();
+        while !self.peek_is('"') && !self.input.finished() {
+            self.input.advance();
         }
         self.peek_expect('"')?;
-        let lit_span = self.lexme_lo_bound.to(self.current.0).updated(1, -1);
+        let lit_span = self
+            .lexme_lo_bound
+            .to(self.input.current().0)
+            .updated(1, -1);
         let lit_val = self.input.spanned(lit_span).into();
         Ok(TokenKind::String(lit_val))
     }
@@ -103,48 +105,31 @@ impl<'s> Scanner<'s> {
 impl<'s> Scanner<'s> {
     /// Creates a new scanner.
     pub fn new(source: &'s str) -> Scanner<'s> {
-        let mut input = Input::new(source);
-        let current = input.next().unwrap_or_default();
         Scanner {
-            input,
-            current,
+            input: Input::new(source),
             lexme_lo_bound: Span::new(0, 0),
         }
-    }
-
-    /// Advances to the next `SpannedChar` in the iterator stream. Returns the new `SpannedChar`.
-    fn advance(&mut self) -> SpannedChar {
-        self.current = self.input.next().unwrap_or_else(|| self.peek());
-        self.current
-    }
-
-    /// Peeks into the next `SpannedChar` in the iterator stream without consuming the current one.
-    fn peek(&mut self) -> SpannedChar {
-        self.input.peek().unwrap_or_else(|| {
-            let len = self.input.source().len();
-            (Span::new(len, len), '\0')
-        })
     }
 
     /// Checks if the next character matches the given one.
     #[inline]
     fn peek_is(&mut self, expected: char) -> bool {
-        self.peek().1 == expected
+        self.input.peek().1 == expected
     }
 
     /// Checks if the next character matches the given one. Will advance in such case.
     fn peek_match(&mut self, expected: char) -> bool {
-        if self.peek().1 != expected {
+        if self.input.peek().1 != expected {
             return false;
         }
-        self.advance();
+        self.input.advance();
         true
     }
 
     /// Checks if the next character matches the given one.
     /// Will advance in such case, otherwise returns an `Err`.
     fn peek_expect(&mut self, expected: char) -> Result<SpannedChar> {
-        let (span, char) = self.peek();
+        let (span, char) = self.input.peek();
         if char != expected {
             bail!(
                 "Unexpected character `{}`, expected `{}` at position {}.",
@@ -153,7 +138,7 @@ impl<'s> Scanner<'s> {
                 span
             );
         }
-        Ok(self.advance())
+        Ok(self.input.advance())
     }
 
     /// Returns `a` if the next character matches the given one. Otherwise returns `b`.
@@ -165,22 +150,16 @@ impl<'s> Scanner<'s> {
         }
     }
 
-    /// Checks if the current token represents the end of the character stream.
-    #[inline]
-    fn is_at_end(&self) -> bool {
-        self.current.1 == '\0'
-    }
-
     /// Creates a new token.
     #[inline]
     fn token(&mut self, kind: TokenKind) -> Token<'s> {
-        let span = self.lexme_lo_bound.to(self.current.0);
+        let span = self.lexme_lo_bound.to(self.input.current().0);
         let token = Token {
             kind,
             lexme: self.input.spanned(span),
             span,
         };
-        self.advance();
+        self.input.advance();
         token
     }
 }
