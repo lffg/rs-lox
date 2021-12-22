01ae89fb33310f3b6446099841682f36457be165
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-12-22 11:08:45 -0300

    Improve scanner error handling in the parser

--- src/parser.rs
+++ src/parser.rs
@@ -48,23 +48,25 @@ pub struct Parser<'src> {
 // Each production has a correspondent method in the following implementation.
 impl Parser<'_> {
     pub fn parse(mut self) -> (Vec<Stmt>, Vec<ParseError>) {
-        // The first advancement:
-        if let Err(err) = self.advance() {
-            self.synchronize_with(err);
-        }
+        (self.parse_program(), self.diagnostics)
+    }
 
+    fn parse_program(&mut self) -> Vec<Stmt> {
         let mut stmts = Vec::new();
         while !self.is_at_end() {
             match self.parse_stmt() {
                 Ok(stmt) => stmts.push(stmt),
-                Err(error) => self.synchronize_with(error),
+                Err(error) => {
+                    self.diagnostics.push(error);
+                    self.synchronize();
+                }
             }
         }
-        (stmts, self.diagnostics)
+        stmts
     }
 
     fn parse_stmt(&mut self) -> PResult<Stmt> {
-        if self.take(TokenKind::Print)? {
+        if self.take(TokenKind::Print) {
             return self.parse_print_stmt();
         }
 
@@ -142,7 +144,7 @@ impl Parser<'_> {
     fn parse_unary(&mut self) -> PResult<Expr> {
         use TokenKind::{Bang, Minus, Show, Typeof};
         if let Bang | Minus | Typeof | Show = self.current_token.kind {
-            let operator = self.advance()?.clone();
+            let operator = self.advance().clone();
             let operand = self.parse_unary()?;
             return Ok(Expr {
                 span: operator.span.to(operand.span),
@@ -159,14 +161,14 @@ impl Parser<'_> {
         use TokenKind::*;
         match self.current_token.kind {
             String(_) | Number(_) | True | False | Nil => {
-                let token = self.advance()?;
+                let token = self.advance();
                 Ok(Expr {
                     kind: expr::Lit::from(token.clone()).into(),
                     span: token.span,
                 })
             }
             LeftParen => {
-                let left_paren_span = self.advance()?.span;
+                let left_paren_span = self.advance().span;
                 let expr = self.parse_expr()?.into();
                 let right_paren_span = self
                     .consume(RightParen, "Expected group to be closed")?
@@ -189,13 +191,15 @@ impl Parser<'_> {
 impl<'src> Parser<'src> {
     /// Creates a new parser.
     pub fn new(src: &'src str) -> Self {
-        Self {
+        let mut parser = Self {
             scanner: Scanner::new(src).peekable(),
             current_token: Token::dummy(),
             prev_token: Token::dummy(),
             diagnostics: Vec::new(),
             options: Default::default(),
-        }
+        };
+        parser.advance(); // The first advancement.
+        parser
     }
 
     /// Checks if the given token kind shall be ignored by this parser.
@@ -206,39 +210,33 @@ impl<'src> Parser<'src> {
     }
 
     /// Advances the parser and returns a reference to the `prev_token` field.
-    fn advance(&mut self) -> PResult<&Token> {
-        self.advance_unchecked();
-        // Handle errors from scanner:
-        if let TokenKind::Error(message) = &self.current_token.kind {
-            return Err(ParseError::ScannerError {
-                message: message.clone(),
-                offending_span: self.current_token.span,
-            });
-        }
-        Ok(&self.prev_token)
-    }
-
-    /// Advances the parser without checking for an `Error` token.
-    fn advance_unchecked(&mut self) {
+    fn advance(&mut self) -> &Token {
         let next = loop {
             let maybe_next = self.scanner.next().expect("Cannot advance past Eof.");
+            // Report `Error` tokens:
+            if let TokenKind::Error(message) = maybe_next.kind {
+                self.diagnostics.push(ParseError::ScannerError {
+                    offending_span: maybe_next.span,
+                    message,
+                });
+                continue; // The parser ignores `Error` tokens.
+            }
             if !Self::is_ignored_kind(&maybe_next.kind) {
                 break maybe_next;
             }
         };
         self.prev_token = mem::replace(&mut self.current_token, next);
+        &self.prev_token
     }
 
     /// Checks if the current token matches the expected one. If so, advances and returns true.
-    /// Otherwise returns false. Such cases are `Ok(bool)`.
-    ///
-    /// Returns `Err` in case of advancement error.
-    fn take(&mut self, expected: TokenKind) -> PResult<bool> {
+    /// Otherwise returns false.
+    fn take(&mut self, expected: TokenKind) -> bool {
         if self.current_token.kind == expected {
-            self.advance()?;
-            return Ok(true);
+            self.advance();
+            return true;
         }
-        Ok(false)
+        false
     }
 
     /// Checks if the current token matches the expected one. If so, advances and returns the
@@ -246,7 +244,7 @@ impl<'src> Parser<'src> {
     /// Also returns `Err` in case of advancement error.
     fn consume(&mut self, expected: TokenKind, msg: impl Into<String>) -> PResult<&Token> {
         if self.current_token.kind == expected {
-            self.advance()
+            Ok(self.advance())
         } else {
             Err(ParseError::UnexpectedToken {
                 message: msg.into(),
@@ -257,41 +255,39 @@ impl<'src> Parser<'src> {
     }
 
     /// Synchronizes the parser state with the current token.
-    ///
     /// A synchronization is needed in order to match the parser state to the current token.
     ///
-    /// When an error is encountered in a `parse_` method, a `ParseError` is returned. These kind of
-    /// errors are forwarded using the `?` operator, which, in practice, unwinds the parser stack
-    /// frame. The question mark operator shall not be used in synchronization points.
-    /// Such synchronization points will call this method, `synchronize_with`.
+    /// When an error is encountered in a `parse_*` method, a `ParseError` is returned. These kind
+    /// of errors are forwarded using the `?` operator, which, in practice, unwinds the parser
+    /// stack frame. Hence the question mark operator should not be used in synchronization points.
+    /// Such synchronization points call this method.
     ///
     /// The synchronization process discards all tokens until it reaches a grammar rule which marks
     /// a synchronization point.
     ///
-    /// In this implementation, synchronization is done in statement boundaries:
+    /// In this implementation, synchronizations are manually performed in statement boundaries:
     ///   * If the previous token is a semicolon, the parser is *probably* (exceptions exists, such
     ///     as a semicolon in a for loop) starting a new statement.
-    ///   * If the next token is a listed (in the implementation) keyword the parser is also
-    ///     starting a new statement.
-    fn synchronize_with(&mut self, error: ParseError) {
-        self.diagnostics.push(error);
-
-        // If the end was already reached there is no need to advance the parser.
+    ///   * If the next token marks the start of a new statement.
+    ///
+    /// Before synchronize one must not forget to emit the raised parse error.
+    fn synchronize(&mut self) {
+        // If the end is already reached any further advancements are needless.
         if self.is_at_end() {
             return;
         }
 
-        self.advance_unchecked();
+        self.advance();
         use TokenKind::*;
         while !{
+            let curr = &self.current_token.kind;
+            let prev = &self.prev_token.kind;
+
             self.is_at_end()
-                || matches!(self.prev_token.kind, Semicolon)
-                || matches!(
-                    self.current_token.kind,
-                    Class | For | Fun | If | Print | Return | Var | While
-                )
+                || matches!(prev, Semicolon)
+                || matches!(curr, Class | For | Fun | If | Print | Return | Var | While)
         } {
-            self.advance_unchecked();
+            self.advance();
         }
     }
 
@@ -307,7 +303,7 @@ macro_rules! bin_expr {
     ($self:expr, kinds = $( $kind:ident )|+, next_production = $next:ident) => {{
         let mut expr = $self.$next()?;
         while let $( TokenKind::$kind )|+ = $self.current_token.kind {
-            let operator = $self.advance()?.clone();
+            let operator = $self.advance().clone();
             let right = $self.$next()?;
             expr = Expr {
                 span: expr.span.to(right.span),
