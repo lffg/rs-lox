eaff7dee7ba58ec26aedad45cc2cc2beb2364831
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2022-02-04 16:22:45 -0300

    Add vm stack

--- vm-lox/src/vm.rs
+++ vm-lox/src/vm.rs
@@ -1,23 +1,42 @@
-use crate::{chunk::Chunk, ins::Ins};
+use crate::{chunk::Chunk, ins::Ins, value::Value};
 
 /// The virtual machine.
-pub struct Vm;
+pub struct Vm {
+    stack: Vec<Value>,
+}
 
 impl Vm {
     pub fn new() -> Vm {
-        Self
+        Self {
+            stack: Vec::with_capacity(256),
+        }
     }
 
     pub fn interpret(&mut self, chunk: Chunk) -> Result<(), ()> {
         for ins in chunk.code {
             use Ins::*;
             match ins {
-                Return => return Ok(()),
+                Return => {
+                    let val = self.pop();
+                    println!("{val:?}");
+                    return Ok(());
+                }
                 Constant(value) => {
-                    println!("{value:?}");
+                    self.push(value);
                 }
             }
         }
         Ok(())
+    }
+
+    fn push(&mut self, value: Value) {
+        if self.stack.len() == self.stack.capacity() {
+            panic!("Stack overflow");
+        }
+        self.stack.push(value);
+    }
+
+    fn pop(&mut self) -> Value {
+        self.stack.pop().unwrap()
     }
 }
