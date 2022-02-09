6b86109d23834c140f3d16a623b02240d8323e54
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2022-02-09 14:22:39 -0300

    Finish scanner
    Ignore comments and whitespaces; add ability to scan for numbers, strings, identifiers and keywords.

--- vm-lox/src/scanner.rs
+++ vm-lox/src/scanner.rs
@@ -2,10 +2,9 @@ use std::str::Chars;
 
 use crate::common::{Span, Token, TokenKind};
 
-const EOF_CHAR: char = '\0';
-
 pub struct Scanner<'s> {
     // Input
+    source: &'s str,
     source_iter: Chars<'s>,
     // Lexme location
     lexme_start: usize,
@@ -22,25 +21,110 @@ impl Scanner<'_> {
 
         use TokenKind::*;
         match current {
+            '\0' => Eof,
+
+            '(' => LeftParen,
+            ')' => RightParen,
+            '{' => LeftBrace,
+            '}' => RightBrace,
+            ';' => Semicolon,
+            ',' => Comma,
+            '.' => Dot,
             '+' => Plus,
             '-' => Minus,
             '*' => Star,
             '/' => Slash,
-            '\0' => Eof,
+
+            '=' => self.next_and('=', EqualEqual, Equal),
+            '!' => self.next_and('=', BangEqual, Bang),
+            '<' => self.next_and('=', LessEqual, Less),
+            '>' => self.next_and('=', GreaterEqual, Greater),
+
+            // Strings
+            '"' => {
+                while self.peek_first() != '"' && !self.is_at_end() {
+                    self.bump();
+                }
+                if self.is_at_end() {
+                    return Error("Unterminated string".into());
+                }
+                self.bump(); // The closing `"`
+                String(self.slice(1, 1).into())
+            }
+
+            // Numbers
+            c if c.is_ascii_digit() => {
+                while self.peek_first().is_ascii_digit() {
+                    self.bump();
+                }
+                if self.peek_first() == '.' && self.peek_second().is_ascii_digit() {
+                    self.bump(); // The `.`
+                    while self.peek_first().is_ascii_digit() {
+                        self.bump();
+                    }
+                }
+                match self.slice(0, 0).parse() {
+                    Ok(number) => Number(number),
+                    Err(_) => Error("Could not parse number literal value".into()),
+                }
+            }
+
+            // Keywords and identifiers
+            c if is_valid_identifier_start(c) => {
+                while is_valid_identifier_tail(self.peek_first()) {
+                    self.bump();
+                }
+                let ident_str = self.slice(0, 0);
+                match KEYWORDS_MAP.get(ident_str) {
+                    Some(keyword_kind) => keyword_kind.clone(),
+                    None => Identifier(ident_str.into()),
+                }
+            }
+
             unexpected => Error(format!("Unexpected token `{unexpected}`.")),
         }
     }
 
     /// Returns the next token in the source string.
     fn scan_token(&mut self) -> Token {
-        // Current `lexme_start` should be the previously produced token's `lexme_end`.
-        // This in fact means the start of a new lexme "registering".
+        // Handle ignored sequences (such as whitespaces and comments) so `scan_kind` does not
+        // have to deal with them.
+        self.skip_whitespace_and_comment();
+
+        // Resets the current lexme start bound (the end bound is automatically handled by `bump`).
         self.lexme_start = self.lexme_end;
 
-        // Calling `scan` must happen before computing the `text` field.
         let kind = self.scan_kind();
         let span = Span::new(self.lexme_start, self.lexme_end);
         Token { kind, span }
+    }
+
+    /// Advances *until* the next token shouldn't be ignored by the scanner.
+    fn skip_whitespace_and_comment(&mut self) {
+        loop {
+            match self.peek_first() {
+                '/' => {
+                    if self.peek_second() == '/' {
+                        while self.peek_first() != '\n' && !self.is_at_end() {
+                            self.bump();
+                        }
+                    } else {
+                        // The first slash must not be consumed if there is no second slash that
+                        // indicates the start of a comment token (which is in fact ignored).
+                        //
+                        // In such case, this function is returned since the first slash must be
+                        // accounted for by the "main" scanner implementation, in `scan_kind`.
+                        return;
+                    }
+                }
+
+                c if c.is_ascii_whitespace() => {
+                    self.bump();
+                }
+
+                _ => return,
+            }
+        }
     }
 }
 
@@ -67,13 +151,40 @@ impl Scanner<'_> {
     }
 
     /// Returns the first next character in the source string without advancing.
-    fn first(&self) -> char {
+    fn peek_first(&self) -> char {
         self.source_iter.clone().next().unwrap_or(EOF_CHAR)
     }
 
     /// Returns the second next character in the source string without advancing.
-    fn second(&self) -> char {
-        self.source_iter.clone().nth(1).unwrap_or(EOF_CHAR)
+    fn peek_second(&self) -> char {
+        let mut iter = self.source_iter.clone();
+        iter.next();
+        iter.next().unwrap_or(EOF_CHAR)
+    }
+
+    /// Returns `a` and advances the scanner if the first next character is equal to the expected
+    /// one. Otherwise returns `b` without advancing.
+    fn next_and<T>(&mut self, expected: char, a: T, b: T) -> T {
+        if self.peek_first() == expected {
+            self.bump();
+            a
+        } else {
+            b
+        }
+    }
+
+    /// Returns a slice over the current lexme bounds. The caller may modify the current bounds:
+    ///   - A `left_modifier` will be *added* to the current *start* bound.
+    ///   - A `right_modifier` will be *subtracted* from the current *end* bound.
+    ///
+    /// Such new computed bound must hold the assertion that `new_start_bound <= new_end_bound`.
+    ///
+    /// While performing the index operation, panics if the computed bound is invalid.
+    fn slice(&self, left_modifier: usize, right_modifier: usize) -> &str {
+        let left = self.lexme_start + left_modifier;
+        let right = self.lexme_end - right_modifier;
+        assert!(left <= left, "Invalid computed bounds");
+        &self.source[left..right]
     }
 
     /// Checks if the source string is finished.
@@ -87,6 +198,7 @@ impl<'s> Scanner<'s> {
     /// Creates a new scanner from the given source string.
     pub fn new(source: &'s str) -> Scanner<'s> {
         Scanner {
+            source,
             source_iter: source.chars(),
             lexme_start: 0,
             lexme_end: 0,
@@ -104,4 +216,37 @@ impl Iterator for Scanner<'_> {
         }
         Some(self.scan_token())
     }
+}
+
+const EOF_CHAR: char = '\0';
+
+static KEYWORDS_MAP: phf::Map<&'static str, TokenKind> = phf::phf_map! {
+    "nil"    => TokenKind::Nil,
+    "true"   => TokenKind::True,
+    "false"  => TokenKind::False,
+    "this"   => TokenKind::This,
+    "super"  => TokenKind::Super,
+    "class"  => TokenKind::Class,
+    "and"    => TokenKind::And,
+    "or"     => TokenKind::Or,
+    "if"     => TokenKind::If,
+    "else"   => TokenKind::Else,
+    "return" => TokenKind::Return,
+    "fun"    => TokenKind::Fun,
+    "for"    => TokenKind::For,
+    "while"  => TokenKind::While,
+    "var"    => TokenKind::Var,
+    "print"  => TokenKind::Print,
+    "typeof" => TokenKind::Typeof,
+    "show"   => TokenKind::Show,
+};
+
+/// Checks if the given char is valid as an identifier's start character.
+fn is_valid_identifier_start(c: char) -> bool {
+    c.is_ascii_alphabetic() || c == '_'
+}
+
+/// Checks if the given char can belong to an identifier's tail.
+fn is_valid_identifier_tail(c: char) -> bool {
+    c.is_ascii_digit() || is_valid_identifier_start(c)
 }
